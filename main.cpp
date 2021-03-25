ba58f0a09ffe3cc6ce06ed2265729bc3d529e624
dimon5360 <dimon5360@gmail.com>
2021-03-26 00:48:30 +0300

    add support of key stop for the server by press key SPACE

--- main.cpp
+++ main.cpp
@@ -18,17 +18,33 @@ const uint32_t PATCH = 1;
 const uint32_t MINOR = 0;
 const uint32_t MAJOR = 0;
 
+#include <windows.h>
+
+/* separate thread for processing of SPACE key press (to close application) */
+static void EscapeWait() {
+    while (GetAsyncKeyState(VK_SPACE) == 0) {
+        Sleep(10);
+    }
+    exit(0);
+}
+
+
 int main()
 {
     /* for corrent output boost error messages */
     SetConsoleOutputCP(1251);
     std::cout << boost::format("Hello. Application version is %1%.%2%.%3%\n") % MAJOR % MINOR % PATCH;
+    std::cout << "Press SPACE to exit...\n";
+
     try
     {
+        /* separate thread to monitor SPACE key pressing */
+        std::thread ext(&EscapeWait);
         /* start tcp server */
         boost::asio::io_service ios;
         async_tcp_server serv(ios);
         ios.run();
+        ext.join();
     }
     catch (std::exception& ex)
     {
